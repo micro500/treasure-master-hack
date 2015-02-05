1be4e96d836c33ddac3a7e756fda193a57fb52bb
pjgat09@gmail.com <pjgat09@gmail.com@e1f7c6db-5fc6-9e08-f73d-102815224cba>
2015-02-05 03:33:42 +0000

    Added BOINC functionality. Intercepted the BOINC functions so BOINC compilation can be turned off easily. With the changes to the project file this project should be able to be dropped into the samples directory of the BOINC source, added to the BOINC windows build solution and compiled in there with the configuration set to "Release BOINC"

---
+++ process_code/process_code/boinc.h
@@ -0,0 +1,41 @@
+#ifndef BOINC_H
+#define BOINC_H
+
+#ifdef BOINCAPP
+
+#ifdef _WIN32
+#include "boinc_win.h"
+#else
+#include "config.h"
+#include <cstdio>
+#include <cctype>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+#include <csignal>
+#include <unistd.h>
+#endif
+
+#include "str_util.h"
+#include "util.h"
+#include "filesys.h"
+#include "boinc_api.h"
+#include "mfile.h"
+
+#define OUTPUT_FILENAME "out"
+
+extern char buf[256], output_path[512];
+extern MFILE output_file;
+
+#endif // BOINCAPP
+
+void initialize_boinc();
+
+void finish_boinc();
+
+void boinc_log(char * fmt, ...);
+
+void fraction_done(double percentage);
+
+
+#endif // BOINC_H
