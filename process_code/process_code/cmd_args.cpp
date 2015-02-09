8b1a6617c3057bdc624b49474573d8ecac375206
pjgat09@gmail.com <pjgat09@gmail.com@e1f7c6db-5fc6-9e08-f73d-102815224cba>
2015-02-09 16:24:02 +0000

    Started adding command line arguments. Added a flag to read a list of IVs from a file.

---
+++ process_code/process_code/cmd_args.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <sstream>
+
+#include "cmd_args.h"
+#include "data_sizes.h"
+
+cli_options command_line_options;
+
+void process_command_line(int argc, char **argv)
+{
+	for (int i = 0; i < argc; i++)
+	{
+		if (std::string(argv[i]) == "-k")
+		{
+			i++;
+
+			std::stringstream ss;
+			ss << std::hex << argv[i];
+			ss >> command_line_options.start_key;
+		}
+		else if (std::string(argv[i]) == "-d")
+		{
+			i++;
+
+			std::stringstream ss;
+			ss << std::hex << argv[i];
+			ss >> command_line_options.start_data;
+		}
+		else if (std::string(argv[i]) == "-c")
+		{
+			i++;
+
+			std::stringstream ss;
+			ss << std::hex << argv[i];
+			ss >> command_line_options.iv_count;
+		}
+		else if (std::string(argv[i]) == "-a")
+		{
+			i++;
+			if (std::string(argv[i]) == "individual")
+			{
+				command_line_options.attack = INDIVIDUAL;
+			}
+			else if (std::string(argv[i]) == "vector")
+			{
+				command_line_options.attack = VECTOR;
+			}
+			else if (std::string(argv[i]) == "hash")
+			{
+				command_line_options.attack = HASH;
+			}
+		}
+		else if (std::string(argv[i]) == "-b")
+		{
+			command_line_options.big_registers = true;
+		}
+		else if (std::string(argv[i]) == "-s")
+		{
+			command_line_options.key_schedule = true;
+		}
+		else if (std::string(argv[i]) == "-f")
+		{
+			command_line_options.from_file = true;
+
+			i++;
+			command_line_options.filename = std::string(argv[i]);
+		}
+	}
+}
---
+++ process_code/process_code/cmd_args.h
@@ -0,0 +1,57 @@
+#ifndef CMD_ARGS_H
+#define CMD_ARGS_H
+
+#include <string>
+
+#include "data_sizes.h"
+
+enum attack_type 
+{
+	INDIVIDUAL,
+	VECTOR,
+	HASH
+};
+
+struct cli_options
+{
+	//  -k, --key "XXXXXXXX": supply 8 hex values to use as the key value, defaults to all 0's?
+	uint32 start_key;
+
+	//  -d, --data "XXXXXXXX": supply 8 hex values to use as the starting data value, defaults to all 0's
+	uint32 start_data;
+
+	//  -c, --count 
+	uint64 iv_count;
+
+	//  -a, --attack attack_type:
+	//               "individual": each IV is processed and checked individually
+	//               "vector":     each working code is added to a std::vector, processed one round, sorted, then std::unique'd
+	//               "hash":       each working code is processed a round, then checked against a hash table. collisions are not processed any further
+	attack_type attack;
+
+	//  -b, --big-registers: use 128bit registers as an optimization
+	bool big_registers;
+
+	//  -s, --key-schedule: pre-process the key schedule as an optimization instead of processing it for every IV
+	bool key_schedule;
+
+	//  -f, --file filename: read a list of IVs to process from a file. Overrides -k and -d
+	bool from_file;
+	std::string filename;
+
+	cli_options() : start_key(0),
+		start_data(0),
+		iv_count(1),
+		attack(INDIVIDUAL),
+		big_registers(false),
+		key_schedule(true),
+		from_file(false),
+		filename(""){}
+
+};
+
+extern cli_options command_line_options;
+
+void process_command_line(int argc, char **argv);
+
+#endif //CMD_ARGS_H
