6a2c55ab7c7041f2f94be7afd707735b69d26e9d
pjgat09 <pjgat09@gmail.com>
2017-07-26 09:37:01 -0400

    Started collecting the RNG functions back into a single file.

--- tm_opencl2/tm_opencl/main.cpp
+++ tm_opencl2/tm_opencl/main.cpp
@@ -4,129 +4,9 @@
 #include <CL/cl.h>
 
 #include "data_sizes.h"
+#include "rng.h"
 
 #define MAX_SOURCE_SIZE (0x100000)
- 
-void generate_rng_table(uint16 * rng_table)
-{
-    for (int i = 0; i <= 0xFF; i++)
-    {
-        for (int j = 0; j <= 0xFF; j++)
-        {
-            unsigned int rngA = i;
-            unsigned int rngB = j;
-            
-            uint8 carry = 0;
-            
-            rngB = (rngB + rngA) & 0xFF;
-            
-            rngA = rngA + 0x89;
-            carry = rngA > 0xFF ? 1 : 0;
-            rngA = rngA & 0xFF;
-            
-            rngB = rngB + 0x2A + carry;
-            carry = rngB > 0xFF ? 1 : 0;
-            rngB = rngB & 0xFF;
-            
-            rngA = rngA + 0x21 + carry;
-            carry = rngA > 0xFF ? 1 : 0;
-            rngA = rngA & 0xFF;
-            
-            rngB = rngB + 0x43 + carry;
-            carry = rngB > 0xFF ? 1 : 0;
-            rngB = rngB & 0xFF;
-            
-            rng_table[(i * 0x100) + j] = (rngA << 8) | rngB;
-        }
-    }
-}
-
-uint8 run_rng(uint16 * rng_seed, uint16 * rng_table)
-{
-	uint16 result = rng_table[*rng_seed];
-	*rng_seed = result;
-
-	return ((result >> 8) ^ (result)) & 0xFF;
-}
-
-
-void generate_regular_rng_values(uint16 * regular_rng_values, uint16 * rng_seed, uint16 * rng_table)
-{
-    for (int i = 0; i < 0x10000; i++)
-    {
-		*rng_seed = i;
-        for (int j = 0; j < 128; j++)
-        {
-            regular_rng_values[i*128 + (127 - j)] = run_rng(rng_seed, rng_table);
-        }
-    }
-}
-
-void generate_alg0_values(uint16 * alg0_values, uint16 * rng_seed, uint16 * rng_table)
-{
-    for (int i = 0; i < 0x10000; i++)
-    {
-        *rng_seed = i;
-        for (int j = 0; j < 128; j++)
-        {
-            alg0_values[i*128 + (127 - j)] = (run_rng(rng_seed, rng_table) >> 7) & 0x01;
-        }
-    }
-}
-
-void generate_alg6_values(uint16 * alg6_values, uint16 * rng_seed, uint16 * rng_table)
-{
-    for (int i = 0; i < 0x10000; i++)
-    {
-        *rng_seed = i;
-        for (int j = 0; j < 128; j++)
-        {
-            alg6_values[i*128 + j] = run_rng(rng_seed, rng_table) & 0x80;
-        }
-    }
-}
-
-void generate_alg2_values(uint32 * alg2_values, uint16 * rng_seed, uint16 * rng_table)
-{
-    for (int i = 0; i < 0x10000; i++)
-    {
-        *rng_seed = i;
-        alg2_values[i] = (run_rng(rng_seed, rng_table) & 0x80) << 9;
-    }
-}
-
-void generate_alg5_values(uint32 * alg5_values, uint16 * rng_seed, uint16 * rng_table)
-{
-    for (int i = 0; i < 0x10000; i++)
-    {
-        *rng_seed = i;
-        alg5_values[i] = (run_rng(rng_seed, rng_table) & 0x80) << 16;
-    }
-}
-
-
-
-void generate_seed_forward_1(uint16 * values, uint16 * rng_table)
-{
-	for (int i = 0; i < 0x10000; i++)
-    {
-		values[i] = rng_table[i];
-    }
-}
-
-void generate_seed_forward_128(uint16 * values, uint16 * rng_seed, uint16 * rng_table)
-{
-	for (int i = 0; i < 0x10000; i++)
-    {
-		*rng_seed = i;
-		for (int j = 0; j < 128; j++)
-		{
-			*rng_seed = rng_table[*rng_seed];
-		}
-		values[i] = *rng_seed;
-    }
-}
-
 
 int main()
 {
@@ -271,12 +151,11 @@ int main()
 	//ret = clSetKernelArg(kernel, 0, 7);
 	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&code_space_d);
 
-	uint16 rng_seed;
 	uint16 * rng_table = new uint16[256*256];
 	generate_rng_table(rng_table);
 
 	uint16 * regular_rng_values = new uint16[0x10000 * 128];
-	generate_regular_rng_values(regular_rng_values, &rng_seed, rng_table);
+	generate_regular_rng_values_16(regular_rng_values, rng_table);
 
 	cl_mem regular_rng_values_d = NULL;
 	regular_rng_values_d = clCreateBuffer(context, CL_MEM_READ_WRITE, 0x10000 * 128 * 2, NULL, &ret);
@@ -296,7 +175,7 @@ int main()
 	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&regular_rng_values_d);
 
 	uint16 * alg0_values = new uint16[0x10000 * 128];
-	generate_alg0_values(alg0_values, &rng_seed, rng_table);
+	generate_alg0_values_16(alg0_values, rng_table);
 
 	cl_mem alg0_values_d = NULL;
 	alg0_values_d = clCreateBuffer(context, CL_MEM_READ_WRITE, 0x10000 * 128 * 2, NULL, &ret);
@@ -316,7 +195,7 @@ int main()
 	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&alg0_values_d);
 
 	uint16 * alg6_values = new uint16[0x10000 * 128];
-	generate_alg6_values(alg6_values, &rng_seed, rng_table);
+	generate_alg6_values_16(alg6_values, rng_table);
 
 	cl_mem alg6_values_d = NULL;
 	alg6_values_d = clCreateBuffer(context, CL_MEM_READ_WRITE, 0x10000 * 128 * 2, NULL, &ret);
@@ -358,7 +237,7 @@ int main()
 
 
 	uint16* rng_seed_forward_128 = new uint16[256*256];
-	generate_seed_forward_128(rng_seed_forward_128, &rng_seed, rng_table);
+	generate_seed_forward_128(rng_seed_forward_128, rng_table);
 	
 	cl_mem rng_seed_forward_128_d = NULL;
 	rng_seed_forward_128_d = clCreateBuffer(context, CL_MEM_READ_WRITE, 256*256*2, NULL, &ret);
@@ -378,7 +257,7 @@ int main()
 
 
 	uint32 * alg2_values = new uint32[0x10000];
-	generate_alg2_values(alg2_values, &rng_seed, rng_table);
+	generate_alg2_values_32(alg2_values, rng_table);
 
 	cl_mem alg2_values_d = NULL;
 	alg2_values_d = clCreateBuffer(context, CL_MEM_READ_WRITE, 0x10000 * 4, NULL, &ret);
@@ -398,7 +277,7 @@ int main()
 	ret = clSetKernelArg(kernel, 6, sizeof(cl_mem), (void *)&alg2_values_d);
 
 	uint32 * alg5_values = new uint32[0x10000];
-	generate_alg5_values(alg5_values, &rng_seed, rng_table);
+	generate_alg5_values_32(alg5_values, rng_table);
 
 	cl_mem alg5_values_d = NULL;
 	alg5_values_d = clCreateBuffer(context, CL_MEM_READ_WRITE, 0x10000 * 4, NULL, &ret);
