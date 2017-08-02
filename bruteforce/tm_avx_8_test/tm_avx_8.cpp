cf235ad7fda050828d844c422f8f361058bf020b
pjgat09 <pjgat09@gmail.com>
2017-08-01 21:20:56 -0400

    Converted AVX_8 to const arguments.

--- bruteforce/tm_avx_8_test/tm_avx_8.cpp
+++ bruteforce/tm_avx_8_test/tm_avx_8.cpp
@@ -5,9 +5,7 @@
 #include <tmmintrin.h> //SSSE3
 #include <smmintrin.h> //SSE4.1
 #include <nmmintrin.h> //SSE4.2
-//#include <ammintrin.h> //SSE4A
 #include <immintrin.h> //AVX
-//#include <zmmintrin.h> //AVX512
 
 #include "data_sizes.h"
 
@@ -16,7 +14,7 @@
         _mm256_castpd_si256(_mm256_insertf128_pd(_mm256_castsi256_pd(_mm256_castsi128_si256(vl)), _mm_castsi128_pd(vh), 1))
 #endif
 
-void alg0(uint8 * working_code, uint8 * alg0_values, uint16 * rng_seed, uint16 * rng_table, uint16 * rng_forward)
+void alg0(uint8 * working_code, const uint8 * alg0_values, const uint16 rng_seed)
 {
 	__m256i mask_FE = _mm256_set1_epi16(0xFEFE);
 
@@ -31,16 +29,15 @@ void alg0(uint8 * working_code, uint8 * alg0_values, uint16 * rng_seed, uint16 *
 		__m256i cur_val = _mm256_set_m128i(cur_val_hi, cur_val_lo);
 		cur_val = _mm256_castpd_si256(_mm256_and_pd (_mm256_castsi256_pd(cur_val), _mm256_castsi256_pd(mask_FE)));
 
-		__m256i rng_val = _mm256_load_si256((__m256i *)(alg0_values + ((*rng_seed) * 128) + (i * 32)));
+		__m256i rng_val = _mm256_load_si256((__m256i *)(alg0_values + (rng_seed * 128) + (i * 32)));
 
 		cur_val = _mm256_castpd_si256(_mm256_or_pd (_mm256_castsi256_pd(cur_val), _mm256_castsi256_pd(rng_val)));
 		
 		_mm256_store_si256 ((__m256i *)(working_code + i*32), cur_val);
     }
-	*rng_seed = rng_forward[*rng_seed];
 }
 
-void alg1(uint8 * working_code, uint8 * regular_rng_values_lo, uint8 * regular_rng_values_hi, uint16 * rng_seed, uint16 * rng_table, uint16 * rng_forward)
+void alg1(uint8 * working_code, const uint8 * regular_rng_values_lo, const uint8 * regular_rng_values_hi, const uint16 rng_seed)
 {
 	__m128i mask_lo = _mm_set1_epi16(0x00FF);
 	__m128i mask_hi = _mm_set1_epi16(0xFF00);
@@ -49,10 +46,10 @@ void alg1(uint8 * working_code, uint8 * regular_rng_values_lo, uint8 * regular_r
     {
 		__m128i cur_val_lo = _mm_loadu_si128((__m128i *)(working_code + i*32));
 		__m128i cur_val_hi = _mm_loadu_si128((__m128i *)(working_code + i*32 + 16));
-		__m128i rng_val_lo_lo = _mm_loadu_si128((__m128i *)(regular_rng_values_lo + ((*rng_seed) * 128) + (i * 32)));
-		__m128i rng_val_lo_hi = _mm_loadu_si128((__m128i *)(regular_rng_values_lo + ((*rng_seed) * 128) + (i * 32) + 16));
-		__m128i rng_val_hi_lo = _mm_loadu_si128((__m128i *)(regular_rng_values_hi + ((*rng_seed) * 128) + (i * 32)));
-		__m128i rng_val_hi_hi = _mm_loadu_si128((__m128i *)(regular_rng_values_hi + ((*rng_seed) * 128) + (i * 32) + 16));
+		__m128i rng_val_lo_lo = _mm_loadu_si128((__m128i *)(regular_rng_values_lo + (rng_seed * 128) + (i * 32)));
+		__m128i rng_val_lo_hi = _mm_loadu_si128((__m128i *)(regular_rng_values_lo + (rng_seed * 128) + (i * 32) + 16));
+		__m128i rng_val_hi_lo = _mm_loadu_si128((__m128i *)(regular_rng_values_hi + (rng_seed * 128) + (i * 32)));
+		__m128i rng_val_hi_hi = _mm_loadu_si128((__m128i *)(regular_rng_values_hi + (rng_seed * 128) + (i * 32) + 16));
 
 		__m128i sum_lo_lo = _mm_and_si128(_mm_add_epi16(_mm_and_si128(cur_val_lo, mask_lo), rng_val_lo_lo), mask_lo);
 		__m128i sum_lo_hi = _mm_and_si128(_mm_add_epi16(_mm_and_si128(cur_val_lo, mask_hi), rng_val_hi_lo), mask_hi);
@@ -67,48 +64,11 @@ void alg1(uint8 * working_code, uint8 * regular_rng_values_lo, uint8 * regular_r
 
 		_mm256_store_si256 ((__m256i *)(working_code + i*32), cur_val);
     }
-	*rng_seed = rng_forward[*rng_seed];
 }
 
-void alg2(uint8 * working_code, uint8 * alg2_values, uint16 * rng_seed, uint16 * rng_table, uint16 * rng_forward)
+void alg2(uint8 * working_code, const uint8 * alg2_values, const uint16 rng_seed)
 {
-	//return;
-	// Load the 256 carry
-
-	// Fetch current working value
-	// Set the next carry by getting lowest of cur_val, shifting to top of 256
-		// do 128bit shift to top of 128 lane, then 256 permute
-
-	// Get extra carry:
-		// do shift to cur val lo, store to temp
-		// permute, do shift to hi, store to temp2
-		// combine into 256
-		// 256 mask it
-		// or with carry
-
-	// final combine
-		// shift cur val lo, store to temp 1
-		// shift cur val hi, store to temp 2
-		// combine into temp large
-		// mask it
-
-		// shift cur val lo, store to temp 1
-		// shift cur val hi, store to temp 2
-		// combine into temp large
-		// mask it
-
-		// Shift across 128 lanes...
-		// shift cur_val hi down 1 cell
-		// shift cur_val hi up 7 cells, keep top 0xFF (probably don't need to mask it) (carry across lanes)
-		// shift cur_val lo down 1 cell, or it with lane carry
-		// combine into a thing
-		// 256 mask it
-
-
-		// 256-or the 4 parts together
-
-
-	__m256i carry = _mm256_load_si256((__m256i *)(alg2_values + ((*rng_seed) * 32)));
+	__m256i carry = _mm256_load_si256((__m256i *)(alg2_values + (rng_seed * 32)));
     for (int i = 3; i >= 0; i--)
     {
 		__m256i cur_val = _mm256_load_si256((__m256i *)(working_code + i*32));
@@ -147,26 +107,24 @@ void alg2(uint8 * working_code, uint8 * alg2_values, uint16 * rng_seed, uint16 *
 
 		carry = next_carry;
     }
-	*rng_seed = rng_forward[*rng_seed];
 }
 
-void alg3(uint8 * working_code, uint8 * regular_rng_values, uint16 * rng_seed, uint16 * rng_table, uint16 * rng_forward)
+void alg3(uint8 * working_code, const uint8 * regular_rng_values, const uint16 rng_seed)
 {
-	uint8 * rng_start = regular_rng_values + ((*rng_seed) * 128);
-	//return;
+	const uint8 * rng_start = regular_rng_values + (rng_seed * 128);
     for (int i = 0; i < 4; i++)
     {
 		__m256i cur_val = _mm256_load_si256((__m256i *)(working_code + i*32));
 		__m256i rng_val = _mm256_load_si256((__m256i *)(rng_start + (i * 32)));
 		cur_val = _mm256_castpd_si256(_mm256_xor_pd (_mm256_castsi256_pd(cur_val), _mm256_castsi256_pd(rng_val)));
+
 		_mm256_store_si256 ((__m256i *)(working_code + i*32), cur_val);
     }
-	*rng_seed = rng_forward[*rng_seed];
 }
 
-void alg5(uint8* working_code, uint8 * alg5_values, uint16 * rng_seed, uint16 * rng_table, uint16 * rng_forward)
+void alg5(uint8* working_code, const uint8 * alg5_values, const uint16 rng_seed)
 {
-	__m256i carry = _mm256_load_si256((__m256i *)(alg5_values + ((*rng_seed) * 32)));
+	__m256i carry = _mm256_load_si256((__m256i *)(alg5_values + (rng_seed * 32)));
     for (int i = 3; i >= 0; i--)
     {
 		__m256i cur_val = _mm256_load_si256((__m256i *)(working_code + i*32));
@@ -206,49 +164,9 @@ void alg5(uint8* working_code, uint8 * alg5_values, uint16 * rng_seed, uint16 * 
 
 		carry = next_carry;
     }
-	*rng_seed = rng_forward[*rng_seed];
-
-
-
-
-
-
-
-	return;
-	/*
-	__m128i carry = _mm_loadu_si128((__m128i *)(alg5_values + ((*rng_seed) * 16)));
-    for (int i = 15; i >= 0; i--)
-    {
-		__m128i cur_val = _mm_loadu_si128((__m128i *)(working_code + i*16));
-		__m128i next_carry = _mm_slli_si128(_mm_and_si128(cur_val,_mm_set_epi16(0,0,0,0,0,0,0,0x80)),14);
-
-		carry = _mm_or_si128(carry, _mm_srli_si128(_mm_and_si128(cur_val,_mm_set_epi16(0,0x80,0,0x80,0,0x80,0,0)),2));
-
-		cur_val = _mm_or_si128(
-			_mm_or_si128(
-				_mm_or_si128(
-					_mm_and_si128(
-						_mm_srli_epi16(cur_val,1), 
-						_mm_set_epi16(0x7f,0,0x7f,0,0x7f,0,0x7f,0)),
-					_mm_and_si128(
-						_mm_slli_epi16(cur_val,1), 
-						_mm_set_epi16(0,0xFE,0,0xFE,0,0xFE,0,0xFE))),
-				_mm_and_si128(
-					_mm_srli_si128(cur_val,2),
-					_mm_set_epi16(0,1,0,1,0,1,0,1))),
-			carry);
-
-		_mm_store_si128 ((__m128i *)(working_code + i*16), cur_val);
-
-		carry = next_carry;
-
-    }
-	*rng_seed = rng_forward[*rng_seed];
-	*/
 }
 
-
-void alg6(uint8 * working_code, uint8 * alg6_values, uint16 * rng_seed, uint16 * rng_table, uint16 * rng_forward)
+void alg6(uint8 * working_code, const uint8 * alg6_values, const uint16 rng_seed)
 {
 	__m256i mask_7F = _mm256_set1_epi16(0x7F7F);
     for (int i = 0; i < 4; i++)
@@ -262,16 +180,13 @@ void alg6(uint8 * working_code, uint8 * alg6_values, uint16 * rng_seed, uint16 *
 		__m256i cur_val = _mm256_set_m128i(cur_val_hi, cur_val_lo);
 		cur_val = _mm256_castpd_si256(_mm256_and_pd (_mm256_castsi256_pd(cur_val), _mm256_castsi256_pd(mask_7F)));
 
-		__m256i rng_val = _mm256_load_si256((__m256i *)(alg6_values + ((*rng_seed) * 128) + (i * 32)));
+		__m256i rng_val = _mm256_load_si256((__m256i *)(alg6_values + (rng_seed * 128) + (i * 32)));
 
 		cur_val = _mm256_castpd_si256(_mm256_or_pd (_mm256_castsi256_pd(cur_val), _mm256_castsi256_pd(rng_val)));
 
 		_mm256_store_si256 ((__m256i *)(working_code + i*32), cur_val);
-
     }
-	*rng_seed = rng_forward[*rng_seed];
 }
-
 
 void alg7(uint8 * working_code)
 {
--- bruteforce/tm_avx_8_test/tm_avx_8_test.cpp
+++ bruteforce/tm_avx_8_test/tm_avx_8_test.cpp
@@ -9,7 +9,6 @@ tm_avx_8_test::tm_avx_8_test()
 	this->rng_table = new uint16[256*256];
 	generate_rng_table(this->rng_table);
 
-
 	regular_rng_values = (uint8*)aligned_malloc(0x10000 * 128, 32);
 	generate_regular_rng_values_8(regular_rng_values, rng_table);
 
@@ -53,35 +52,40 @@ void tm_avx_8_test::process_test_case(uint8 * test_case, uint16 * rng_seed, int 
 		working_code[i] = test_case[i];
 	}
 
-	this->rng_seed = *rng_seed;
-
 	if (algorithm == 0)
 	{
-		alg0((uint8*)working_code, (uint8*)alg0_values, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+		alg0((uint8*)working_code, (uint8*)alg0_values, *rng_seed);
+		*rng_seed = rng_seed_forward_128[*rng_seed];
 	}
 	else if (algorithm == 1)
 	{
-		alg1((uint8*)working_code, (uint8*)regular_rng_values_lo, (uint8*)regular_rng_values_hi, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+		alg1((uint8*)working_code, (uint8*)regular_rng_values_lo, (uint8*)regular_rng_values_hi, *rng_seed);
+		*rng_seed = rng_seed_forward_128[*rng_seed];
 	}
 	else if (algorithm == 2)
 	{
-		alg2((uint8*)working_code, (uint8*)alg2_values, &(this->rng_seed), this->rng_table, rng_seed_forward_1);
+		alg2((uint8*)working_code, (uint8*)alg2_values, *rng_seed);
+		*rng_seed = rng_seed_forward_1[*rng_seed];
 	}
 	else if (algorithm == 3)
 	{
-		alg3((uint8*)working_code, (uint8*)regular_rng_values, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+		alg3((uint8*)working_code, (uint8*)regular_rng_values, *rng_seed);
+		*rng_seed = rng_seed_forward_128[*rng_seed];
 	}
 	else if (algorithm == 4)
 	{
-		alg1((uint8*)working_code, (uint8*)alg4_values_lo, (uint8*)alg4_values_hi, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+		alg1((uint8*)working_code, (uint8*)alg4_values_lo, (uint8*)alg4_values_hi, *rng_seed);
+		*rng_seed = rng_seed_forward_128[*rng_seed];
 	}
 	else if (algorithm == 5)
 	{
-		alg5((uint8*)working_code, (uint8*)alg5_values, &(this->rng_seed), this->rng_table, rng_seed_forward_1);
+		alg5((uint8*)working_code, (uint8*)alg5_values, *rng_seed);
+		*rng_seed = rng_seed_forward_1[*rng_seed];
 	}
 	else if (algorithm == 6)
 	{
-		alg6((uint8*)working_code, (uint8*)alg6_values, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+		alg6((uint8*)working_code, (uint8*)alg6_values, *rng_seed);
+		*rng_seed = rng_seed_forward_128[*rng_seed];
 	}
 	else if (algorithm == 7)
 	{
@@ -92,8 +96,6 @@ void tm_avx_8_test::process_test_case(uint8 * test_case, uint16 * rng_seed, int 
 	{
 		test_case[i] = working_code[i];
 	}
-
-	*rng_seed = this->rng_seed;
 }
 
 void tm_avx_8_test::run_iterations(uint8 * test_case, uint16 * rng_seed, int algorithm, int iterations)
@@ -105,56 +107,60 @@ void tm_avx_8_test::run_iterations(uint8 * test_case, uint16 * rng_seed, int alg
 		working_code[i] = test_case[i];
 	}
 
-	this->rng_seed = *rng_seed;
-
-
 	if (algorithm == 0)
 	{
 		for (int i = 0; i < iterations; i++)
 		{
-			alg0((uint8*)working_code, (uint8*)alg0_values, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+			alg0((uint8*)working_code, (uint8*)alg0_values, *rng_seed);
+			*rng_seed = rng_seed_forward_128[*rng_seed];
 		}
 	}
 	else if (algorithm == 1)
 	{
 		for (int i = 0; i < iterations; i++)
 		{
-			alg1((uint8*)working_code, (uint8*)regular_rng_values_lo, (uint8*)regular_rng_values_hi, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+			alg1((uint8*)working_code, (uint8*)regular_rng_values_lo, (uint8*)regular_rng_values_hi, *rng_seed);
+			*rng_seed = rng_seed_forward_128[*rng_seed];
 		}
 	}
 	else if (algorithm == 2)
 	{
 		for (int i = 0; i < iterations; i++)
 		{
-			alg2((uint8*)working_code, (uint8*)alg2_values, &(this->rng_seed), this->rng_table, rng_seed_forward_1);
+			alg2((uint8*)working_code, (uint8*)alg2_values, *rng_seed);
+			*rng_seed = rng_seed_forward_1[*rng_seed];
 		}
 	}
 	else if (algorithm == 3)
 	{
 		for (int i = 0; i < iterations; i++)
 		{
-			alg3((uint8*)working_code, (uint8*)regular_rng_values, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+			alg3((uint8*)working_code, (uint8*)regular_rng_values, *rng_seed);
+			*rng_seed = rng_seed_forward_128[*rng_seed];
 		}
 	}
 	else if (algorithm == 4)
 	{
 		for (int i = 0; i < iterations; i++)
 		{
-			alg1((uint8*)working_code, (uint8*)alg4_values_lo, (uint8*)alg4_values_hi, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+			alg1((uint8*)working_code, (uint8*)alg4_values_lo, (uint8*)alg4_values_hi, *rng_seed);
+			*rng_seed = rng_seed_forward_128[*rng_seed];
 		}
 	}
 	else if (algorithm == 5)
 	{
 		for (int i = 0; i < iterations; i++)
 		{
-			alg5((uint8*)working_code, (uint8*)alg5_values, &(this->rng_seed), this->rng_table, rng_seed_forward_1);
+			alg5((uint8*)working_code, (uint8*)alg5_values, *rng_seed);
+			*rng_seed = rng_seed_forward_1[*rng_seed];
 		}
 	}
 	else if (algorithm == 6)
 	{
 		for (int i = 0; i < iterations; i++)
 		{
-			alg6((uint8*)working_code, (uint8*)alg6_values, &(this->rng_seed), this->rng_table, rng_seed_forward_128);
+			alg6((uint8*)working_code, (uint8*)alg6_values, *rng_seed);
+			*rng_seed = rng_seed_forward_128[*rng_seed];
 		}
 	}
 	else if (algorithm == 7)
